84b9f43c08d65a1fc38af8c59d19922d69210467
Alessandro Jeronimo Passafaro <contato@alessandropassafaro.com.br>
2022-11-10 10:20:46 -0300

    Dados Redes Sociais
    Array onde armazena os dados de redes sociais foi transferido para o controlador

--- html/application/controllers/Welcome.php
+++ html/application/controllers/Welcome.php
@@ -6,7 +6,15 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 
-		$dados = array('telas' => array('home'));
+		$redesSociais = array(
+			'whatsapp'=>'https://api.whatsapp.com/send?phone=5544999933625&text=Ol%C3%A1%20estou%20visitando%20o%20website%20e%20gostaria%20de%20tirar%20algumas%20d%C3%BAvidas',
+			'github'=>'https://github.com/passafaro/',
+			'instagram'=>'https://www.instagram.com/alessandropassafaro/',
+			'linkedin'=>'',
+			'facebook'=>'https://www.facebook.com/alessandro.passafaro.3'
+		);
+
+		$dados = array('telas' => array('home'), 'redesSociais' => $redesSociais);
 		$this->load->view('principal', $dados);
 
 	}
--- html/application/views/home.php
+++ html/application/views/home.php
@@ -1,14 +1,5 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed'); 
-
-$redesSociais = array(
-    'whatsapp'=>'https://api.whatsapp.com/send?phone=5544999933625&text=Ol%C3%A1%20estou%20visitando%20o%20website%20e%20gostaria%20de%20tirar%20algumas%20d%C3%BAvidas',
-    'github'=>'https://github.com/passafaro/',
-    'instagram'=>'https://www.instagram.com/alessandropassafaro/',
-    'linkedin'=>'',
-    'facebook'=>'https://www.facebook.com/alessandro.passafaro.3'
-);
-
 ?>
 
 
